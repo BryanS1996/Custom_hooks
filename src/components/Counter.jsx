a87b47d18e5dc9521f546ebb37c6ecf59dfce1c9
Bryan Chileno <bryanfabricio96@hotmail.com>
2025-12-01 10:38:01 -0500

    structure change

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useCounter } from "../hooks/useCounter";
+
+export function Counter() {
+  const { count, increment, decrement, reset } = useCounter(5);
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
